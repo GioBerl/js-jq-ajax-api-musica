1f714d06090e6dac2fce1db86322cf75a17eee3c
Gio <giovanni.berl1@gmail.com>
2020-05-20 16:35:44 +0200

    first commit fatto esercizio manca bonus

---
+++ main.js
@@ -0,0 +1,36 @@
+// Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
+// https://flynn.boolean.careers/exercises/api/array/music
+// Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
+
+// BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
+$(document).ready(function () {
+    //recupero html del disco e creo la funzione di handlebars
+    var source = $("#disco-template").html();
+    var templateFunc = Handlebars.compile(source);
+
+    //vado a richiedere i dati che mi servono
+    $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        method: "GET",
+        success: function (data) {
+            dischi = data.response;
+            //per ogni disco in dischi applico il suo contesto
+            dischi.forEach(function (disco) {
+                console.log(disco.genre);
+                //a sinistra seleziono il placeholder di handlebars a destra il dato recuperato
+                var context = {
+                    genre: disco.genre,
+                    poster: disco.poster,
+                    title: disco.title,
+                    author: disco.author,
+                    year: disco.year,
+                };
+                //compilo il context
+                var newHtml = templateFunc(context);
+                //e lo appendo al contenitore html
+                $(".cds-container").append(newHtml);
+            });
+        },
+        error: function () {},
+    });
+});
